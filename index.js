bc2054fa34b1c8f0fea09f0e6816c91d8b3792d2
kingwooram <rkddnkfa1006@naver.com>
2025-05-29 02:06:16 +0900

    123

---
+++ index.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    const singerInfo = {
+        "공민지": { name: "공민지", bio: "2NE1 출신, 솔로 활동 중" },
+        "윤딴딴": { name: "윤딴딴", bio: "싱어송라이터, '니가 보고 싶은 밤' 등" },
+        "팔로알토": { name: "팔로알토", bio: "힙합 뮤지션, Hi-Lite Records 설립자" },
+        "이재환": { name: "이재환", bio: "K-POP 교수, 전 JYP 트레이너" }
+    };
+
+    $(".circle").hover(
+        function () {
+            const $circle = $(this);
+            const $img = $circle.find("img");
+            const id = $img.attr("id");
+            const info = singerInfo[id];
+
+            // 스타일 변화 (불투명하게)
+            $img.css("opacity", "0.5");
+
+            if ($circle.find(".info-text").length === 0) {
+                $circle.append(`
+                    <div class="info-text">
+                        이름 : ${info.name}<br>
+                        이력 : ${info.bio}
+                    </div>
+                `);
+            }
+        },
+        function () {
+            const $circle = $(this);
+            const $img = $circle.find("img");
+
+            // 이미지 다시 밝게
+            $img.css("opacity", "1");
+
+            // 텍스트 제거
+            $circle.find(".info-text").remove();
+        }
+    );
+});
--- 김석진2.js
+++
@@ -1,3 +0,0 @@
-var sname = "김석진";
-document.write(sname);
-window.alert(sname);
